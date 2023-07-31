1054c6fd006b5f77573dfaa2a36bfe064cfc0b5d
dexorded <126596523+dexorded@users.noreply.github.com>
2023-07-31 14:08:39 +0300

    fix: rename home page

--- lib/src/presentation/pages/home_page.dart
+++ lib/src/presentation/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomePagr extends StatelessWidget {
-  const HomePagr({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
