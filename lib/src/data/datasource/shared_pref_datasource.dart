d1f9f1b9e5d2532505dd0040fc2af8c3959f1298
dexorded <126596523+dexorded@users.noreply.github.com>
2023-07-31 16:18:04 +0300

    feat: add datasource for shared pref

---
+++ lib/src/data/datasource/shared_pref_datasource.dart
@@ -0,0 +1,41 @@
+import 'package:project/core/error/shared_pref_errors.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefDatasource {
+  SharedPreferences? sharedPreferences;
+
+  Future<void> saveString({
+    required String key,
+    required String value,
+  }) async {
+    await _init();
+
+    if (sharedPreferences != null) {
+      await sharedPreferences?.setString(key, value);
+    }
+
+    throw SharedPreferencesNotInitializated();
+  }
+
+  Future<String> getString({
+    required String key,
+  }) async {
+    await _init();
+
+    if (sharedPreferences != null) {
+      final String? data = sharedPreferences?.getString(key);
+
+      if (data != null) {
+        return data;
+      }
+
+      throw SharedPreferencesDoesNotHaveData();
+    }
+
+    throw SharedPreferencesNotInitializated();
+  }
+
+  Future<void> _init() async {
+    sharedPreferences ??= await SharedPreferences.getInstance();
+  }
+}
